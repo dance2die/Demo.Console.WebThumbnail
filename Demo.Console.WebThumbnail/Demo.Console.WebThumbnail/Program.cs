faf11e13a3172a6ab37978fa7c069f0e6d3e560c
dance2die <dontbothermewithspam@gmail.com>
2015-04-14 23:09:46 -0400

    For some reason, thumbnail is created 6 times!!!

--- Demo.Console.WebThumbnail/Demo.Console.WebThumbnail/Program.cs
+++ Demo.Console.WebThumbnail/Demo.Console.WebThumbnail/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
 using System.Threading.Tasks;
 
 namespace Demo.Console.WebThumbnail
@@ -10,6 +8,18 @@ namespace Demo.Console.WebThumbnail
 	{
 		static void Main(string[] args)
 		{
+			CreateThumbnail();
+		}
+
+		private static async Task CreateThumbnail()
+		{
+			//string url = string.Format("http://{0}", "weitzlux.com");
+			string url = "http://www.charith.gunasekara.web-sphere.co.uk/2010/09/how-to-convert-html-page-to-image-using.html";
+			int thumbnailWidth = 800;
+			int thumbnailHeight = 600;
+			Thumbnail thumbnail = new Thumbnail(url, 800, 600, thumbnailWidth, thumbnailHeight);
+			Bitmap image = await thumbnail.GenerateThumbnailAsync();
+			image.Save(@"c:\temp\Thumbnail.jpg", ImageFormat.Jpeg);
 		}
 	}
 }
